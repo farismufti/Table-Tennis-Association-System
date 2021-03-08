12cc4fac468642167f9a1d297d1c288c85966cec
Faris Mufti <faris.mufti@outlook.com>
2021-03-08 18:25:26 +0300

    First commit'
    '
    
    quit
    exit
    git

---
+++ Match.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Match {
+
+    protected String homeTeam;
+    protected  String awayTeam;
+    protected Set set1;
+    protected Set set2;
+    protected Set set3;
+    protected Set set4;
+    protected DoubleSet doubleSet;
+    protected int finalScore;
+
+    public Match() {}
+
+    public Match(String homeTeam, String awayTeam) {
+
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+
+    }
+}
